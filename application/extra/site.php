6766bb6f30f1ec03d89c6f8079cf5daca3a32582
wong1989 <skyandsunning@163.com>
2018-05-26 19:51:05 +0800

    add site config

---
+++ application/extra/site.php
@@ -0,0 +1,42 @@
+<?php
+
+return array (
+  'name' => '会计服务',
+  'beian' => '皖ICP备15054802号-4',
+  'cdnurl' => '',
+  'version' => '1.0.1',
+  'timezone' => 'Asia/Shanghai',
+  'forbiddenip' => '',
+  'languages' => 
+  array (
+    'backend' => 'zh-cn',
+    'frontend' => 'zh-cn',
+  ),
+  'fixedpage' => 'general/profile',
+  'categorytype' => 
+  array (
+    'normal' => '普通',
+    'service' => '服务',
+    'article' => '文章',
+  ),
+  'configgroup' => 
+  array (
+    'basic' => 'Basic',
+    'email' => 'Email',
+    'dictionary' => 'Dictionary',
+    'user' => 'User',
+    'example' => 'Example',
+  ),
+  'mail_type' => '1',
+  'mail_smtp_host' => 'smtp.qq.com',
+  'mail_smtp_port' => '465',
+  'mail_smtp_user' => '10000',
+  'mail_smtp_pass' => 'password',
+  'mail_verify_type' => '2',
+  'mail_from' => '10000@qq.com',
+  'description' => '专业服务，实惠便捷，用最高的效率最好的态度服务每一个客户',
+  'logo' => '/uploads/20180329/6a69cbf3ffa9b0dcb862fe7cf4681025.jpg',
+  'wechat' => '/uploads/20180329/a5165e10040e2e588e70c05106c7db25.jpg',
+  'tel' => '012-3445555',
+  'mobile' => '1569890987',
+);
